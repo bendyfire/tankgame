ab4aae0fc5258c6789f4497f471d0d93054a602a
camps <camps@camps072.local>
2017-11-10 14:33:56 -0700

    no message

--- javatank.js
+++ javatank.js
@@ -2,14 +2,34 @@
 alert("_______WELCOME TO ?????_______")
 alert("-----------RULES--------------")
 
+w = 600;
+h = 400;
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 100, 100);
-
 var firstKill = prompt("")
-if (firstkill == "high b"){
+if (firstKill == "high b"){
 	alert("You Hit The Tank!")
 }
 
+var drawGrid = function(w, h) {
+    ctx.canvas.width  = w;
+    ctx.canvas.height = h;
+
+
+    for (x=0;x<=w;x+=100) {
+        for (y=0;y<=h;y+=100) {
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, h);
+            ctx.stroke();
+            ctx.moveTo(0, y);
+            ctx.lineTo(w, y);
+            ctx.stroke();
+        }
+    }
+
+};
+    drawGrid(500, 500);
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 100, 100);
