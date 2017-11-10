8bd5873597c2b0025695f520940134e6c8a24724
kyle <camps@camps068.local>
2017-11-10 14:05:02 -0700

    no message

---
+++ javatank.js
@@ -0,0 +1,15 @@
+
+alert("_______WELCOME TO ?????_______")
+alert("-----------RULES--------------")
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 100, 100);
+
+var firstKill = prompt("")
+if (firstkill == "high b"){
+	alert("You Hit The Tank!")
+}
+
