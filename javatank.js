a61aec39b6ff9d57d46ab0fe8e3bfa96083b02c0
ben <bendyfire@squaredev.com>
2017-11-11 00:02:31 -0700

    no message

--- javatank.js
+++ javatank.js
@@ -17,12 +17,19 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var scoreDiv = document.getElementById("score")
 var attack = document.getElementById("attack");
-
+var score = 0
 
 function background() {
 	var img = new Image();
 	img.src = 'photos/New Piskel (2).png'
 	ctx.drawImage(img, 0, 0, 500, 500)
+}
+
+
+function teleport(){
+	enemy_position.x = Math.floor(Math.random()*5)*100+5
+	enemy_position.y = Math.floor(Math.random()*5)*100+5
+	enemy_position.armor = Math.floor(Math.random()*3) 
 }
 
 var image = document.getElementById('source');
@@ -31,24 +38,24 @@ var image = document.getElementById('source');
 
 
 var drawGrid = function(w, h) {
-    ctx.canvas.width  = w;
-    ctx.canvas.height = h;
+	ctx.canvas.width  = w;
+	ctx.canvas.height = h;
 
-    background();
+	background();
 
-    for (x=0;x<=w;x+=100) {
-        for (y=0;y<=h;y+=100) {
-            ctx.moveTo(x, 0);
-            ctx.lineTo(x, h);
-            ctx.stroke();
-            ctx.moveTo(0, y);
-            ctx.lineTo(w, y);
-            ctx.stroke();
-        }
-    }
+	for (x=0;x<=w;x+=100) {
+		for (y=0;y<=h;y+=100) {
+			ctx.moveTo(x, 0);
+			ctx.lineTo(x, h);
+			ctx.stroke();
+			ctx.moveTo(0, y);
+			ctx.lineTo(w, y);
+			ctx.stroke();
+		}
+	}
 
 };
-    drawGrid(500, 500);
+drawGrid(500, 500);
 
 function draw_player(x, y) {
 	var img = new Image();
@@ -78,22 +85,22 @@ function draw_enemyleft(x, y) {
 	var img = new Image();
 	img.src = 'photos/badTank.png';
 	ctx.drawImage(img, x, y, 90, 90)};
-function draw_enemyright(x, y) {
-	var img = new Image();
-	img.src = 'photos/badTank clone.png';
-	ctx.drawImage(img, x, y, 90, 90)};
-function draw_enemyup(x, y) {
-	var img = new Image();
-	img.src = 'photos/New Piskel (7).png';
-	ctx.drawImage(img, x, y, 90, 90)};
-function draw_enemydown(x, y) {
-	var img = new Image();
-	img.src = 'photos/New Piskel (8).png';
-	ctx.drawImage(img, x, y, 90, 90)};
+	function draw_enemyright(x, y) {
+		var img = new Image();
+		img.src = 'photos/badTank clone.png';
+		ctx.drawImage(img, x, y, 90, 90)};
+		function draw_enemyup(x, y) {
+			var img = new Image();
+			img.src = 'photos/New Piskel (7).png';
+			ctx.drawImage(img, x, y, 90, 90)};
+			function draw_enemydown(x, y) {
+				var img = new Image();
+				img.src = 'photos/New Piskel (8).png';
+				ctx.drawImage(img, x, y, 90, 90)};
 
 
 
-draw_player(5,5)
+				draw_player(5,5)
 
 //for (var i = 0; i < numEnemies; i++) Math.floor(Math.random()) {
 
@@ -111,16 +118,26 @@ function move(e){
 			player_position.x += 100;
 			draw_enemyright(enemy_position.x,enemy_position.y)
 			document.getElementById("p1").innerHTML = "enter ammo";
-			attack.value
+			attack.addEventListener
+			if (attack.value >= enemy_position.armor){
+				score += 100
+				scoreDiv.innerHTML = 'Score: ' + score;
+				draw_enemyleft(enemy_position.x, enemy_position.y)
+				teleport()
+			}
 
 		}
-				
+
 		if (player_position.x<0) {
 			player_position.x = 5;
 		}
 		draw_playerleft( player_position.x, player_position.y);
 
 	}
+
+
+
+
 	//right
 	if(e.keyCode == 39) {
 		ctx.clearRect(x, y, ctx.canvas.width, ctx.canvas.height);
@@ -131,8 +148,13 @@ function move(e){
 			player_position.x -= 100;
 			draw_enemyleft(enemy_position.x,enemy_position.y)
 			document.getElementById("p1").innerHTML = "enter ammo";
-			attack.value
-
+			attack.addEventListener
+			if (attack.value >= enemy_position.armor){
+				score += 100
+				scoreDiv.innerHTML = 'Score: ' + score;
+				draw_enemyleft(enemy_position.x, enemy_position.y)
+				teleport()
+			}
 		}
 
 		if (player_position.x>500) {
@@ -140,6 +162,8 @@ function move(e){
 		}
 		draw_player( player_position.x, player_position.y);
 	}
+
+
 	//down
 	if(e.keyCode == 40) { 
 		ctx.clearRect(x, y, ctx.canvas.width, ctx.canvas.height);
@@ -150,7 +174,13 @@ function move(e){
 			player_position.y -= 100;
 			draw_enemyup(enemy_position.x,enemy_position.y)
 			document.getElementById("p1").innerHTML = "enter ammo";
-			attack.value
+			attack.addEventListener
+			if (attack.value >= enemy_position.armor){
+				score += 100
+				scoreDiv.innerHTML = 'Score: ' + score;
+				draw_enemyleft(enemy_position.x, enemy_position.y)
+				teleport()
+			}
 		}	
 		if (player_position.y>500) {
 			player_position.y = 405;
@@ -158,6 +188,8 @@ function move(e){
 		}
 		draw_playerdown( player_position.x, player_position.y);
 	}
+
+
 	//up
 	if(e.keyCode == 38) {
 		ctx.clearRect(x, y, ctx.canvas.width, ctx.canvas.height);
@@ -167,36 +199,26 @@ function move(e){
 			player_position.y += 100;
 			draw_enemydown(enemy_position.x,enemy_position.y)
 			document.getElementById("p1").innerHTML = "enter ammo";
-			attack.value
+			attack.addEventListener
+			if (attack.value >= enemy_position.armor){
+				score += 100
+				scoreDiv.innerHTML = 'Score: ' + score;
+				draw_enemyleft(enemy_position.x, enemy_position.y)
+				teleport()
 			}
+		}
 
 		if (player_position.y<0) {
 			player_position.y = 5;
 		}
 		draw_playerup( player_position.x, player_position.y);
 	}
+}
 	
 
-}
 
-document.onkeydown = move;
 
-//var level = {
-//	1: 0,
-//	2: 1000,
-//	3: 2000,
-//	4: 3000,
-//	5: 4000}
-//function get_level(points){
-//    for (level in levels){
-//       if (points < levels[level]){
-//          return level - 1;
-//      }
-//    }
-//}
-//function getLevel(point){
-//	var level = -1 + Math.sqrt(4 + points/20);
-//	scoreDiv.innerHTML = "Score : " + points;
-//	return Math.floor(level);
-//}
+	document.onkeydown = move;
+
+
 
