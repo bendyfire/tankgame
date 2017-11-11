6222a6e54c5658a96529e27ff65be716a8659428
kyle <camps@camps068.local>
2017-11-10 18:39:35 -0700

    no message

--- javatank.js
+++ javatank.js
@@ -8,6 +8,9 @@ h = 400;
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+var image = document.getElementById('source');
+
+ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
 
 
 var drawGrid = function(w, h) {
